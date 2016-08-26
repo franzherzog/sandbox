d55b6d00bc9ebb8ac2d4c70ce7cee602a66788df
franzherzog <kontakt@franzherzog.de>
2016-08-26 16:35:01 +0200

    new reactnative project

--- service-worker/sw.js
+++ service-worker/sw.js
@@ -1,3 +1,10 @@
-self.addEventListener('activated',function(data){
+self.addEventListener('activate',function(data){
     console.log('activated')
+})
+self.addEventListener('push', function(event) {
+  event.waitUntil(
+    self.registration.showNotification('Example Notification', {
+      body: 'Hello, world!',
+    })
+  );
 })
